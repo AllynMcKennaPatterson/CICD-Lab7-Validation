78c9318207a643b8addc87a60dc2782f6ba5ce59
G00392765@atu.ie <G00392765@atu.ie>
2023-11-15 10:05:49 +0000

    Added getPersonById

--- src/main/java/atu/cicd/lab7_validation/PersonService.java
+++ src/main/java/atu/cicd/lab7_validation/PersonService.java
@@ -7,4 +7,9 @@ public class PersonService {
     public void savePerson(Person person){
         System.out.println("Person saved: " + person);
     }
+
+    public Person getPersonByEmployeeId(String employeeId){
+        Person myPerson = new Person("Allyn", "Mr", employeeId, 22, "G00392765@atu.ie", "student", "engineering");
+        return myPerson;
+    }
 }
