b513e3e3518dc2228689624ab9bb75619e14f839
G00392765@atu.ie <G00392765@atu.ie>
2023-11-15 09:49:07 +0000

    Added bindingResult to return error

--- src/main/java/atu/cicd/lab7_validation/PersonController.java
+++ src/main/java/atu/cicd/lab7_validation/PersonController.java
@@ -1,20 +1,20 @@
 package atu.cicd.lab7_validation;
 
 import jakarta.validation.Valid;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
-    private Person person;
 
-    public PersonController(Person person){
-        this.person = person;
-    }
 
     @PostMapping("createPerson")
-    public Person addPerson(@Valid @RequestBody Person person){
-        return person;
+    public String addPerson(@Valid @RequestBody Person person, BindingResult bindingResult){
+        if(bindingResult.hasErrors())
+            return bindingResult.toString();
+        System.out.println("Person created: " + person);
+        return "Person added:" + person;
     }
 }
