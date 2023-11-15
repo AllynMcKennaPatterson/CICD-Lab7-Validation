1b9cb88fc4022c8b1f957ae113bdb2a4a7fcea6c
G00392765@atu.ie <G00392765@atu.ie>
2023-11-15 09:40:23 +0000

    Added PostMapping to createPerson

--- src/main/java/atu/cicd/lab7_validation/PersonController.java
+++ src/main/java/atu/cicd/lab7_validation/PersonController.java
@@ -1,4 +1,20 @@
 package atu.cicd.lab7_validation;
 
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PersonController {
+    private Person person;
+
+    public PersonController(Person person){
+        this.person = person;
+    }
+
+    @PostMapping("createPerson")
+    public Person addPerson(@Valid @RequestBody Person person){
+        return person;
+    }
 }
