935a4c7c77175312e5780518c3e0df0fe188c7dc
G00392765@atu.ie <G00392765@atu.ie>
2023-11-15 09:55:04 +0000

    Added savePerson method

--- src/main/java/atu/cicd/lab7_validation/PersonService.java
+++ src/main/java/atu/cicd/lab7_validation/PersonService.java
@@ -4,4 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
+    public void savePerson(Person person){
+        System.out.println("Person saved: " + person);
+    }
 }
