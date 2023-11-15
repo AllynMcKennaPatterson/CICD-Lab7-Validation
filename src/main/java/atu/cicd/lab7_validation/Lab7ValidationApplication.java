3e35c98062f5c4b185e0df4af7399a3c33b4a828
G00392765@atu.ie <G00392765@atu.ie>
2023-11-15 09:22:38 +0000

    Initial Commit

---
+++ src/main/java/atu/cicd/lab7_validation/Lab7ValidationApplication.java
@@ -0,0 +1,13 @@
+package atu.cicd.lab7_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7ValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab7ValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/atu/cicd/lab7_validation/Lab7ValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.cicd.lab7_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7ValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
